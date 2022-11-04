7f379d6f56a4dd722cbc02f36e5937de29ad636a
shafikul <kingshafikul@gmail.com>
2022-11-04 11:31:25 +0600

    slider end

--- js/slider.js
+++ js/slider.js
@@ -10,9 +10,13 @@ right.addEventListener('click', ()=>{
     if(slideNumber < length){
         slider.style.transform = `translateX(-${slideNumber * 800}px)`;
         slideNumber++;
+        resetBg();
+        buttons[slideNumber - 1].style.backgroundColor = 'white';
     }else{
         slider.style.transform = `translateX(0px)`;
         slideNumber = 1;
+        resetBg();
+        buttons[slideNumber - 1].style.backgroundColor = 'white';
     }
     
 });
@@ -21,9 +25,13 @@ left.addEventListener('click', ()=>{
     if(slideNumber > 1){
         slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`;
         slideNumber--;
+        resetBg();
+        buttons[slideNumber - 1].style.backgroundColor = 'white';
     }else{
         slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
         slideNumber = length;
+        resetBg();
+        buttons[slideNumber - 1].style.backgroundColor = 'white';
     }
            
 });
@@ -37,7 +45,19 @@ for(var i = 0; i < length; i++){
 }
 
 const buttons = document.querySelectorAll('.button');
+buttons[0].style.backgroundColor = "white";
+
+const resetBg = ()=>{
+    buttons.forEach(button=>{
+        button.style.backgroundColor = 'transparent';
+    })
+}
 
 buttons.forEach((button, index)=>{
-
+    button.addEventListener('click', ()=>{
+        resetBg();
+        slider.style.transform = `translateX(-${index * 800}px)`;
+        slideNumber = index + 1;
+        button.style.backgroundColor = "white";
+    });
 })
